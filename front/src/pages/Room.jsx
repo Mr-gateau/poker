afe133b693923efecf7920eaa6b9c86893dcac5d
mr-gateau <tagot.nicolas@gmail.com>
2022-10-22 16:51:26 +0200

    draft create router

--- front/src/pages/Room.jsx
+++ front/src/pages/Room.jsx
@@ -2,14 +2,15 @@ import { useParams } from "react-router-dom";
 import socketIO from "socket.io-client";
 
 const Room = () => {
-  const socket = socketIO.connect("http://localhost:5000");
   const params = useParams();
+  const socket = socketIO.connect("http://localhost:5000/");
   console.log("room", params.room);
   const numberSuite = [1, 2, 3, 4];
 
   const send = (numb) => {
     console.log("send", numb);
     socket.emit(
+      // IDENTIFY BEIN ROOM ?
       "chat message",
       JSON.stringify({
         room: params.room,
